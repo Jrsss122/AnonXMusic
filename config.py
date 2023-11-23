c369457d896e0a0d0961cc0f7d2fb011797972fc
J Robott <115394912+Jrsss122@users.noreply.github.com>
2023-11-24 01:58:12 +0700

    .
    Signed-off-by: J Robott <115394912+Jrsss122@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AnonymousX1025/AnonXMusic",
+    "https://github.com/Jrsss122/JarsssXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
