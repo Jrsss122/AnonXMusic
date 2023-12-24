79e114f6746169278ae71b84902879946c4cc73b
Xysz <115394912+Jrsss122@users.noreply.github.com>
2023-12-24 21:10:28 +0700

    .
    Signed-off-by: Xysz <115394912+Jrsss122@users.noreply.github.com>

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
+    "GIT_TOKEN", "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"
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Chjarsss1")
